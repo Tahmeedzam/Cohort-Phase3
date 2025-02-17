9e352fa0faa0ad277c8d0d9ae934f0e8585d69c3
Tahmeedzam <tahmeedzamindar.tz@gmail.com>
2025-02-17 16:58:19 +0530

    Creating homePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
+import Navbar from './components/navbar';
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
 
 const App = () => {
-  const name = "Tahmeed";
-  const x = 10;
-  const y = 20;
-  const names = ['Tahmeed', 'Vishhal', 'Jayesh'];
-  const styles = {
-    color: 'red',
-    fontSize: '55px'
-  }
-
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <h3 style={styles}>Hello {name} </h3>
-      <h5>Sum of {x} and {y} = {x + y}</h5>
-
-      <ul>
-        {names.map((name, index) => (
-          <li key={index} >{name}</li>
-        ))}
-      </ul>
+      <Navbar />
+      <Hero />
+      <HomeCards />
     </div>
   )
 }
